2b4bbca9d2f96d83d36aacee22891ab929391ceb
butterflyforkill <vasse.paul@gmail.com>
2024-09-21 13:44:06 +0200

    Add models, start to configure the project

---
+++ app/models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=True)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, default=func.now())
+
+    telegram_tokens = relationship("TelegramToken", backref="user")
+    subscriptions = relationship("UserSubscription", backref="user")
+    notifications = relationship("UserNotification", backref="user")
+
+class TelegramToken(Base):
+    __tablename__ = "telegram_tokens"
+
+    token_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    telegram_token = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, default=func.now())
+
+class SubscribeCategory(Base):
+    __tablename__ = "subscribe_categories"
+
+    category_id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String, nullable=True)
+
+class UserSubscription(Base):
+    __tablename__ = "user_subscriptions"
+
+    subscription_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    category_id = Column(Integer, ForeignKey("subscribe_categories.category_id"))
+    created_at = Column(TIMESTAMP, default=func.now())
+
+class UserNotification(Base):
+    __tablename__ = "user_notifications"
+
+    notification_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    notification_time = Column(Time, nullable=False)
+    created_at = Column(TIMESTAMP, default=func.now())
---
+++ config/config_files.py
@@ -0,0 +1,11 @@
+import os
+from dataclasses import dataclass
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+@dataclass(frozen=True)
+class APIkeys:
+    TOKENkey: str = os.getenv('token')
+    APIkey: str = os.getenv('apikey')
+    DATABASEpass: int = os.getenv('databasepass')
