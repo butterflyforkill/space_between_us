96d7fd96dd4d44be4ce5dae241f5fd73b1d9881a
butterflyforkill <vasse.paul@gmail.com>
2024-09-24 00:00:15 +0200

    Plan the future routes

--- app/NASA_api_handler/handler_NASA_API.py
+++ app/NASA_api_handler/handler_NASA_API.py
@@ -6,37 +6,6 @@ from config.config_files import APIkeys
 APOD_URL = 'https://api.nasa.gov/planetary/apod?api_key=' + APIkeys.APIkey
 
 
-def save_to_json(data, filename='nasa_data.json'):
-    with open(filename, 'w') as outfile:
-        json.dump(data, outfile)
-  
-
-def fetch_data(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
-
-def check_status(response):
-    if response.status_code == 200:
-        print("Message has been sent.")
-        return True
-    else:
-        print("Message hasn't been sent.")
-        return False
-
-
-def process_data(response_text):
-    data_dict = json.loads(response_text)
-    processed_data = {}
-    for key, value in data_dict.items():
-        print(f"{key}:")
-        print(f"    {value}")
-        print("-" * 50)
-        processed_data[key] = value
-    return processed_data
-
-
 def get_nasa_data(date=None):
     url = APOD_URL
     if date:
--- app/main.py
+++ app/main.py
@@ -16,3 +16,55 @@ def get_db():
          db.close()
 
 
+# routes
+
+@app.get('/')
+async def home():
+    """
+    render the template of the website 
+    where we will have the registration button, login button
+    some beautiful images how it's working
+    """
+    pass
+
+# auth routes
+
+@app.post('/sign_up')
+async def sign_up():
+    pass
+
+@app.post('/sign_in')
+async def sign_in():
+    pass
+
+# routes that will be visible after registration and login
+
+@app.get('/profile/{int:user_id}')
+async def user_profile(user_id):
+    """
+    shows the user profile where user
+    can sign in into his telegram profile
+
+    Args:
+        user_id (_type_): _description_
+    """
+    pass
+
+
+@app.get('/subscription_categories')
+async def subscription_list():
+    """
+    the list of categories for subscribe
+    """
+    pass
+
+@app.route('/subscription_categories/{int:categorie_id}', methods=['GET', 'POST'])
+async def subscribe(categorie_id):
+    """
+    user choose the categorie and subscribe to it 
+    in the form where he can put the time when he want to receive news
+
+    Args:
+        categorie_id (_type_): _description_
+    """
+    pass
