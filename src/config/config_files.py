72dd7dad8ec791ff94ce34ba797d0052ed6e2c30
butterflyforkill <vasse.paul@gmail.com>
2024-10-10 18:05:22 +0200

    Add redis client and functionality

--- src/config/config_files.py
+++ src/config/config_files.py
@@ -11,3 +11,5 @@ class Settings:
     DATABASEpass: str = os.getenv('databasepass')
     JWT_SECRET_KEY: str = os.getenv('jwt_secret_key')
     JWT_ALGORITHM: str = os.getenv('jwt_algorithm')
+    REDIS_HOST: str = os.getenv('redis_host')
+    REDIS_PORT: int = os.getenv('redis_port')
---
+++ src/db/redis.py
@@ -0,0 +1,23 @@
+import aioredis
+from src.config.config_files import Settings
+
+
+JTI_EXPIRY = 3600
+
+token_blocklist = aioredis.StrictRedis(
+    host=Settings.REDIS_HOST,
+    port=Settings.REDIS_PORT,
+    db=0
+)
+
+
+async def add_jti_to_blocklist(jti: str) -> None:
+    await token_blocklist.set(
+        name=jti,
+        value="",
+        ex=JTI_EXPIRY
+    )
+
+
+async def token_in_blocklist(jti: str) -> bool:
+    return await token_blocklist.get(jti) is not None
